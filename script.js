cd823fafd18634d1ee9f93b143992cef1734bf59
underpig <forbfp3@gmail.com>
2025-06-05 21:32:32 +0300

    добавил переменные, которые позволяют выбрать колво квадратов. пофиксил баг с закрашиванием

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ let btn = document.getElementById('click');
 let state =[];
 let mouseX;
 let mouseY;
+let canvasSize = 500;
+let squareAmount = 10;
+let squareSize = canvasSize / squareAmount;
+
 
 
 btn.onclick = function() {
     state = [];
-    
+    //написно делать вертикальные
     ctx.clearRect(0,0,500,500);
-    
-    for (let y = 0; y < 5; y += 1) {
+
+    for (let y = 0; y < canvasSize; y += 1) {
         state.push([])
-        
-        for (let i = 0; i < 5; i += 1) {
+
+        for (let i = 0; i < canvasSize; i += 1) {
             let random = Math.random();
             if (random < 0.5) {
-               state[y].push(true);
-               ctx.fillRect(i * 100, y * 100, 100, 100);
-                
+                state[y].push(true);
+                ctx.fillRect(i * squareSize , y * squareSize, squareSize, squareSize);
+
             } else {
-                ctx.strokeRect(i * 100, y * 100, 100, 100); 
+                ctx.strokeRect(i * squareSize, y * squareSize, squareSize, squareSize);
                 state[y].push(false);
             }
         }
@@ -30,20 +34,23 @@ btn.onclick = function() {
 
 
 canvas.onclick = function(event) {
-    mouseX = Math.floor(event.offsetX/100);
-    mouseY = Math.floor(event.offsetY/100);
-    
-    if (state[mouseY][mouseX] == false) {
+    mouseX = Math.floor(event.offsetX/squareSize);
+    mouseY = Math.floor(event.offsetY/squareSize);
+
+    if (state[mouseY][mouseX] === false) {
         state[mouseY][mouseX] = true;
-        ctx.clearRect(mouseX * 100, mouseY * 100, 100, 100);
-        ctx.fillRect(mouseX * 100, mouseY * 100, 100, 100);
+        //alert (mouseX);
+        ctx.clearRect(mouseX * squareSize, mouseY * squareSize, squareSize, squareSize);
+        ctx.fillRect(mouseX * squareSize, mouseY * squareSize, squareSize, squareSize);
+
         mouseX = null;
         mouseY = null;
 
     } else {
         state[mouseY][mouseX] = false;
-        ctx.clearRect(mouseX * 100, mouseY * 100, 100, 100);
-        ctx.strokeRect(mouseX * 100, mouseY * 100, 100, 100);
+        //alert ('черная');
+        ctx.clearRect(mouseX * squareSize, mouseY * squareSize, squareSize, squareSize);
+        ctx.strokeRect(mouseX * squareSize, mouseY * squareSize, squareSize, squareSize);
         mouseX = null;
         mouseY = null;
     }
